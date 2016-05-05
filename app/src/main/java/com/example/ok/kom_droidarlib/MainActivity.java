c246b34b781dc8b4b75410e50568c57859c7e290
Oleg Kozin <ok@redwerk.com>
2016-05-05 16:05:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ok/kom_droidarlib/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ok.kom_droidarlib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import gl.GL1Renderer;
+import gl.GLFactory;
+import system.ArActivity;
+import system.DefaultARSetup;
+import worldData.World;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Button button = new Button(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ArActivity.startWithSetup(MainActivity.this, new DefaultARSetup() {
+                    @Override
+                    public void addObjectsTo(GL1Renderer renderer, World world, GLFactory objectFactory) {
+                        world.add(objectFactory.newHexGroupTest(null));
+                    }
+                });
+            }
+        });
+        setContentView(button);
+    }
+}
